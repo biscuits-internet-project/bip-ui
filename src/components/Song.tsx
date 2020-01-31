566a4dbb12fa923737c6b90f14bb3ec352c64c0c
Don Cote <mistercote@gmail.com>
2020-01-31 03:43:06 -0500

    show every time song played on the song page

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import axios, { AxiosResponse } from 'axios'
+import { IVenue } from './Venues';
 
 interface ISong {
 	id: string,
@@ -17,17 +18,36 @@ interface ISong {
 	last_time_played: Date
 }
 
+interface ISongPlayed {
+	annotations: string[]
+	position: number
+	segue: string
+	set: string
+	venue: IVenue
+	show: IShow
+}
+
+interface IShow {
+	date: Date
+	notes?: string
+	slug: string
+}
 
 const Song: React.FC = () => {
 
 	const params = useParams();
 	const [loading, setLoading] = useState(false)
 	const [song, setSong] = useState<ISong | undefined>(undefined)
+	const [songsPlayed, setSongsPlayed] = useState<ISongPlayed[]>([])
 	useEffect(()=> {
 		setLoading(true)
 		const fetchSong = async () => {
-			const data:AxiosResponse = await axios.get(`https://stg-api.discobiscuits.net/api/songs/${params.id}`)
-			setSong(data.data)
+			const song:AxiosResponse = await axios.get(`https://stg-api.discobiscuits.net/api/songs/${params.id}`)
+			setSong(song.data)
+
+			const songs:AxiosResponse = await axios.get(`https://stg-api.discobiscuits.net/api/tracks/songs/${params.id}`)
+			setSongsPlayed(songs.data)
+
 			setLoading(false)
 		}
 		fetchSong()
@@ -43,6 +63,28 @@ const Song: React.FC = () => {
 					<div>last time played: {song.last_time_played}</div>
 				</div>
 			}
+
+				<table>
+				<thead>
+					<tr>
+						<td>Date</td>
+						<td>Venue</td>
+						<td>Set</td>
+						<td>City</td>
+						<td>State</td>
+					</tr>
+				</thead>
+
+				{songsPlayed.map((s: ISongPlayed) => {
+					 return <tr>
+								<td>{s.show.date}</td>
+								<td>{s.venue.name}</td>
+								<td>{s.set}</td>
+								<td>{s.venue.city}</td>
+								<td>{s.venue.state}</td>
+							</tr>
+				})}
+			</table>
 		</>
 	)
 }
