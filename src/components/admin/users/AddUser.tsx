717caceabd6fd91ece3034703c18be686a9febfb
shaun Hutch <shaun@promoboxx.com>
2020-02-02 16:42:39 -0500

    Simple errors (#32)
    * fix
    
    * simple error handling
    
    * success

--- src/components/admin/users/AddUser.tsx
+++ src/components/admin/users/AddUser.tsx
@@ -5,6 +5,8 @@ import {AppContext} from '../../../context/AppProvider'
 import TextField from '../../shared/TextField'
 import {IUser} from './AdminUsers'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 interface IAddUser {
     updateUsers(user: IUser): void
@@ -20,9 +22,10 @@ const initialValues = {
   last_name: "",
   avatar: ""
 }
-const postUser = async (values: IUser, actions:FormikHelpers<IUser>, updateUsers: (user: IUser) => void, token: string | null) => {
-    console.log(values)
-    const newUser:AxiosResponse = await axios({
+const postUser = async (values: IUser, actions:FormikHelpers<IUser>, updateUsers: (user: IUser) => void, token: string | null, setError, setSuccess) => {
+    try{
+      setError(null)
+      const newUser:AxiosResponse = await axios({
         method: 'post',
         url: 'https://stg-api.discobiscuits.net/api/users',
         data: values,
@@ -30,41 +33,57 @@ const postUser = async (values: IUser, actions:FormikHelpers<IUser>, updateUsers
             "Content-Type":	"application/json",
             "Authorization": token
         }
-    });
-    
-    updateUsers(newUser.data)
-    updateUsers(values)
-    actions.resetForm()
+      });
+      setSuccess(true)
+      updateUsers(newUser.data)
+    }
+    catch(error){
+      const {response} = error
+      //Could be as granular as we like here or could leave it generic.
+      console.log(response)
+      setError(response.statusText)
+    }
 }
 
 const AddUser: React.FC<IAddUser> = ({updateUsers}) => {
     const {state} = useContext(AppContext)
+    const [error, setError] = useState(null)
+    const [success, setSuccess] = useState(false)
     return (
         <div>
-          <Formik
+          {success ? (<Formik
             initialValues={initialValues}
-            onSubmit={(values, actions) => postUser(values, actions, updateUsers, state.token)}
+            onSubmit={(values, actions) => postUser(values, actions, updateUsers, state.token, setError, setSuccess)}
           >
             {(props: FormikProps<IUser>) => (
               <Form>
-                 <TextField name="email" type="email" label="Email" />
+                <TextField name="email" type="email" label="Email" />
                 <TextField name="username" type="text" label="Username" />
                 <TextField name="first_name" type="text" label="First Name" />
                 <TextField name="last_name" type="text" label="Last Name" />
                 <TextField name="password" type="password" label="Password" />
                 <TextField name="password_confirmation" type="password" label="Password Confirmation" />
 
-                // add image avatar upload to rails here //
+                <p>add image avatar upload to rails here</p>
                 <input name="avatar" type="file"></input>
-
-                <div style={{display: 'flex', justifyContent: 'flex-end', marginTop: '16px'}}>
+                {error && <Typography color="error" variant="h6">{error}</Typography>}
+                <div style={{display: 'flex', justifyContent: 'flex-end', marginTop: '16px'}}> 
                   <Button variant="contained" color="primary" type="submit">
                     Submit
                   </Button>
                 </div>
               </Form>
             )}
-          </Formik>
+          </Formik>) : (
+            <div style={{width: '100%', height: '600px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+              <CheckCircleOutlineIcon color="secondary" fontSize='large'/>
+              <Typography  variant="h4">Success!</Typography>
+              <Typography  variant="h6">User has been successfully been created</Typography>
+                <Button variant="contained" color="primary" style={{marginTop: '32px'}}onClick={()=>setSuccess(!success)}>
+                    Create Another User
+                </Button>
+            </div>
+          )}
         </div>
       );
 }
