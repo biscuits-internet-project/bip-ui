06d073a16895cfa351048964429292b46730705c
Don Cote <mistercote@gmail.com>
2020-02-02 17:17:34 -0500

    add response handling to identity requests

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Formik, FormikProps} from 'formik'
 import axios, { AxiosResponse } from 'axios'
 import TextField from './shared/TextField'
 import * as Yup from 'yup';
+import { Typography } from '@material-ui/core';
 
 const ForgotPasswordSchema = Yup.object().shape({
   email: Yup.string()
@@ -18,7 +19,9 @@ const initialValues: IForgotPassword = {
     email: "",
 }
 
-const postForgotPassword = async (values: IForgotPassword) => {
+const postForgotPassword = async (values: IForgotPassword, setError, setSuccess) => {
+  try {
+    setError(null)
     const forgotPasswordRequest:AxiosResponse = await axios({
         method: 'post',
         url: 'https://stg-api.discobiscuits.net/api/auth/password/reset',
@@ -27,25 +30,40 @@ const postForgotPassword = async (values: IForgotPassword) => {
             "Content-Type":	"application/json", 
         }
     });
+    setSuccess(true)
     return forgotPasswordRequest.data
+  }
+  catch(error) {
+    const {response} = error
+    //Could be as granular as we like here or could leave it generic.
+    console.log(response)
+    setError(response.statusText)
+  }
 }
 
 const ForgotPassword: React.FC = () => {
+    const [error, setError] = useState(null)
+    const [success, setSuccess] = useState(false)
     return (
         <div>
           <h1>Forgot Password</h1>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={(values) => postForgotPassword(values)}
-            validationSchema={ForgotPasswordSchema}
-          >
-            {(props: FormikProps<IForgotPassword>) => (
-              <Form>
-                <TextField name="email" type="email" label="Email" />
-                <button type="submit">Submit</button>
-              </Form>
-            )}
-          </Formik>
+          {success ? (
+            <div>Please check your email and click the link to rest your password.</div>
+          ) : (
+            <Formik
+              initialValues={initialValues}
+              onSubmit={(values) => postForgotPassword(values, setError, setSuccess)}
+              validationSchema={ForgotPasswordSchema}
+            >
+              {(props: FormikProps<IForgotPassword>) => (
+                <Form>
+                  {error && <Typography color="error" variant="h6">{error}</Typography>}
+                  <TextField name="email" type="email" label="Email" />
+                  <button type="submit">Submit</button>
+                </Form>
+              )}
+            </Formik>
+          )}
         </div>
       );
 }
