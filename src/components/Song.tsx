6f7e1082ce7c3d7f5aee8cbc88c6aa1e93bf9add
Don Cote <mistercote@gmail.com>
2020-02-16 15:55:31 -0500

    clean up song page, add relisten links

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link as RouterLink, useParams } from 'react-router-dom';
 import axios, { AxiosResponse } from 'axios'
 import { IVenue } from './Venues';
 import { Helmet } from "react-helmet";
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { Link, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import Moment from 'react-moment';
 
 interface ISong {
@@ -17,8 +17,8 @@ interface ISong {
 	tabs: string,
 	title: string,
 	times_played: number,
-	first_time_played: Date,
-	last_time_played: Date,
+	first_played_show: IShow,
+	last_played_show: IShow,
 	history: string,
 	featured_lyric: string
 }
@@ -37,6 +37,8 @@ interface IShow {
 	date: Date
 	notes?: string
 	slug: string
+	venue: IVenue
+	relisten_url?: string
 }
 
 const Song: React.FC = () => {
@@ -45,6 +47,7 @@ const Song: React.FC = () => {
 	const [loading, setLoading] = useState(false)
 	const [song, setSong] = useState<ISong | undefined>(undefined)
 	const [songsPlayed, setSongsPlayed] = useState<ISongPlayed[]>([])
+
 	useEffect(()=> {
 		setLoading(true)
 		const fetchSong = async () => {
@@ -69,10 +72,69 @@ const Song: React.FC = () => {
 					<div>
 						<h1>{song.title}</h1>
 						<div><em>{song.featured_lyric}</em></div>
-						<div>author: {song.author_name}</div>
-						<div>number of times played: {song.times_played}</div>
-						<div>first time played: {song.first_time_played}</div>
-						<div>last time played: {song.last_time_played}</div>
+
+						<TableContainer component={Paper}>
+							<Table>
+								<TableRow>
+									<TableCell>
+										Author
+									</TableCell>
+									<TableCell>
+										{song.author_name}
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										Times placyed
+									</TableCell>
+									<TableCell>
+										{song.times_played}
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										Debut
+									</TableCell>
+									<TableCell>
+										<Link component={RouterLink} to={`/shows/${song.first_played_show.slug}`}>
+											<Moment format="MMMM D, YYYY">
+												{song.first_played_show.date}
+											</Moment>
+											<span> - </span>
+											{song.first_played_show.venue.name}
+											<span> - </span>
+											{song.first_played_show.venue.city},
+											{song.first_played_show.venue.state}
+											<span> </span>
+										</Link>
+
+										{song.first_played_show.relisten_url &&
+											<Link href={song.first_played_show.relisten_url} target="blank">
+												<img src="/relisten.png" alt="relisten" />
+											</Link>
+										}
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										Last played
+									</TableCell>
+									<TableCell>
+										<Link component={RouterLink} to={`/shows/${song.last_played_show.slug}`}>
+											<Moment format="MMMM D, YYYY">
+												{song.last_played_show.date}
+											</Moment>
+											<span> - </span>
+											{song.last_played_show.venue.name}
+											<span> - </span>
+											{song.last_played_show.venue.city},
+											{song.last_played_show.venue.state}
+										</Link>
+									</TableCell>
+								</TableRow>
+							</Table>
+						</TableContainer>
+
 						{song.history &&
 							<>
 								<h3>History</h3>
@@ -98,6 +160,7 @@ const Song: React.FC = () => {
 						<TableCell>Venue</TableCell>
 						<TableCell>City</TableCell>
 						<TableCell>State</TableCell>
+						<TableCell>Relisten</TableCell>
 						<TableCell>Notes</TableCell>
 					</TableRow>
 					</TableHead>
@@ -105,19 +168,27 @@ const Song: React.FC = () => {
 						{songsPlayed.map((s: ISongPlayed) => (
 							<TableRow>
 								<TableCell component="th" scope="row">
-									<Link to={`/shows/${s.show.slug}`} >
+									<Link component={RouterLink} to={`/shows/${s.show.slug}`} >
 										<Moment format="MMMM D, YYYY">
 											{s.show.date}
 										</Moment>
 									</Link>
 								</TableCell>
 								<TableCell>
-									<Link to={`/venues/${s.venue.slug}`}>
+									<Link component={RouterLink} to={`/venues/${s.venue.slug}`}>
 										{s.venue.name}
 									</Link>
 								</TableCell>
+
 								<TableCell>{s.venue.city}</TableCell>
 								<TableCell>{s.venue.state}</TableCell>
+								<TableCell>
+									{s.show.relisten_url &&
+										<Link href={s.show.relisten_url} target="blank">
+											<img src="/relisten.png" alt="relisten" />
+										</Link>
+									}
+								</TableCell>
 								<TableCell>{s.note}</TableCell>
 							</TableRow>
 						))}
