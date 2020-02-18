be440b2840e7bdfbe01a5b8b359d637469b4ebc1
Don Cote <mistercote@gmail.com>
2020-02-18 02:43:05 -0500

    options for show card icons/functionality

--- src/components/Setlist.tsx
+++ src/components/Setlist.tsx
@@ -3,11 +3,14 @@ import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Tracklist, { ITrack } from './Tracklist';
 import Moment from 'react-moment';
-import { Typography, Paper, Link, Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, CardActions, SvgIconProps, SvgIcon } from '@material-ui/core';
+import { Typography, Paper, Link, Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, CardActions, SvgIconProps, SvgIcon, FormControlLabel, Switch, Badge, Tooltip, Checkbox, Grid } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import BeenhereIcon from '@material-ui/icons/Beenhere';
 import HeadsetIcon from '@material-ui/icons/Headset';
-
+import { red, green } from '@material-ui/core/colors';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ConfirmationNumberIcon from '@material-ui/icons/ConfirmationNumber';
+import RateReviewIcon from '@material-ui/icons/RateReview';
 export interface ISetlist {
 	slug: string
 	notes: string
@@ -28,9 +31,9 @@ const useStyles = makeStyles((theme: Theme) =>
 	}),
 );
 
+
 const Setlist: React.FC<ISetlist> = ({ date, slug, venue, tracks, notes }) => {
 	const classes = useStyles();
-
 	return (
 		<Card className={classes.root}>
 			<CardHeader
@@ -54,17 +57,65 @@ const Setlist: React.FC<ISetlist> = ({ date, slug, venue, tracks, notes }) => {
 				<Tracklist tracks={tracks}></Tracklist>
 			</CardContent>
 			<CardActions disableSpacing>
-				<IconButton aria-label="Like">
-					<FavoriteIcon />
+
+			<FormControlLabel
+				value="bottom"
+				control={<Switch color="primary" />}
+				label="Was there"
+				labelPlacement="bottom"
+			/>
+
+			<FormControlLabel
+				value="bottom"
+				control={<Switch color="primary" />}
+				label="Like"
+				labelPlacement="bottom"
+			/>
+
+				<IconButton aria-label="Favorite">
+					<Badge badgeContent={4} color="default">
+					<FavoriteIcon
+						style={{color: red[500] }}
+					/>
+					</Badge>
 				</IconButton>
 
 				<IconButton aria-label="I was there">
-					<BeenhereIcon />
+					<Badge badgeContent={4} color="default">
+					<ThumbUpIcon />
+					</Badge>
 				</IconButton>
+				 <Tooltip title="I was there" aria-label="add">
+					<IconButton aria-label="I was there">
+						<ConfirmationNumberIcon style={{ color: green[500] }} />
+					</IconButton>
+				</Tooltip>
 
 				<IconButton aria-label="Relisten">
 					<HeadsetIcon />
 				</IconButton>
+
+
+				<FormControlLabel
+        control={
+          <Checkbox
+            checked={false}
+            value=""
+            color="default"
+          />
+        }
+        label="I was there"
+      />
+				<FormControlLabel
+        control={
+          <Checkbox
+            checked={true}
+            value=""
+            color="primary"
+          />
+        }
+        label="Like"
+      />
 			</CardActions>
 		</Card>
 	)
