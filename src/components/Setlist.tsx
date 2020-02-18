ef9bae6d48ff17a334a275df23945615dbad5ea4
Don Cote <mistercote@gmail.com>
2020-02-17 21:22:00 -0500

    build out setlist card

--- src/components/Setlist.tsx
+++ src/components/Setlist.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Tracklist, { ITrack } from './Tracklist';
 import Moment from 'react-moment';
-import { Typography, Paper, Link } from '@material-ui/core';
+import { Typography, Paper, Link, Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, CardActions, SvgIconProps, SvgIcon } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import BeenhereIcon from '@material-ui/icons/Beenhere';
+import HeadsetIcon from '@material-ui/icons/Headset';
 
 export interface ISetlist {
 	slug: string
@@ -12,29 +16,57 @@ export interface ISetlist {
 	tracks: ITrack[]
 }
 
-var setlistStyle = {
-	padding: 10,
-	marginBottom: 20
-};
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			marginBottom: 20,
+		},
+		media: {
+			height: 0,
+			paddingTop: '56.25%', // 16:9
+		},
+	}),
+);
 
-const Setlist: React.FC<ISetlist> = ({date,slug,venue,tracks,notes}) => {
+const Setlist: React.FC<ISetlist> = ({ date, slug, venue, tracks, notes }) => {
+	const classes = useStyles();
 
 	return (
-		<Paper elevation={3} style={setlistStyle}>
-			<Typography>
-				<Link component={RouterLink} to={`/shows/${slug}`}>
-					<Moment format="MMMM D, YYYY">
-						{date}
-					</Moment>
-				</Link>
-				<span> - </span>
-				<Link component={RouterLink} to={`/venues/${venue.slug}`}>
-					{venue.name} - {venue.city}, {venue.state}
-				</Link>
-			</Typography>
-			<Typography variant="body2" dangerouslySetInnerHTML={{__html: notes}} />
-			<Tracklist tracks={tracks}></Tracklist>
-		</Paper>
+		<Card className={classes.root}>
+			<CardHeader
+				title = {
+					<Link component={RouterLink} to={`/shows/${slug}`}>
+						<Moment format="MMMM D, YYYY">
+							{date}
+						</Moment>
+					</Link>
+				}
+				subheader = {venue &&
+					<>
+						<Link component={RouterLink} to={`/venues/${venue.slug}`}>
+							{venue.name} - {venue.city}, {venue.state}
+						</Link>
+					</>
+				}
+			/>
+			<CardContent style={{paddingTop: 0, paddingBottom: 0}}>
+				<Typography variant="body2" dangerouslySetInnerHTML={{ __html: notes }} />
+				<Tracklist tracks={tracks}></Tracklist>
+			</CardContent>
+			<CardActions disableSpacing>
+				<IconButton aria-label="Like">
+					<FavoriteIcon />
+				</IconButton>
+
+				<IconButton aria-label="I was there">
+					<BeenhereIcon />
+				</IconButton>
+
+				<IconButton aria-label="Relisten">
+					<HeadsetIcon />
+				</IconButton>
+			</CardActions>
+		</Card>
 	)
 }
 
