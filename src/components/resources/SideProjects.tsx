5201c78c0305fcf364ead07561961180651a7aa0
Don Cote <mistercote@gmail.com>
2020-02-19 16:24:45 -0500

    add side projects

--- src/components/resources/SideProjects.tsx
+++ src/components/resources/SideProjects.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from "react-helmet";
 import PageHeading from '../shared/PageHeading';
+import axios, { AxiosResponse } from 'axios'
+import { Card, CardHeader, Typography, CardContent, Grid } from '@material-ui/core';
+
+interface ISideProject {
+	notes?: string,
+	dates: string,
+	name: string,
+	members: string[]
+}
 
 const SideProjects: React.FC = () => {
+	const [sideProjects, setSideProjects] = useState<ISideProject[]>([])
+
+	useEffect(() => {
+		const fetchSideProjects = async () => {
+			const data:AxiosResponse = await axios.get(`${process.env.REACT_APP_API_URL}/side_projects`)
+			setSideProjects(data.data)
+		}
+		fetchSideProjects()
+	}, [])
 	return (
 		<>
 			<Helmet>
 				<title>Biscuits Internet Project - Side Projects</title>
 			</Helmet>
 			<PageHeading text="Side Projects"/>
+			<Grid container spacing={3}  alignItems="stretch">
+				{sideProjects.map((sp) => {
+					return (
+						<Grid item xs={12} sm={6} md={4} style={{display: 'flex', width: 350}}>
+							<Card style={{display: 'flex', width: "100%"}}>
+								<CardContent>
+									<Typography variant="h3">{sp.name}</Typography>
+									<Typography>{sp.dates}</Typography>
+									{sp.members.map((mem) => {
+											return <div key={mem}> {mem}</div>
+									})}
+								</CardContent>
+							</Card>
+						</Grid>
+					)
+				})}
+			</Grid>
 		</>
 	)
 }
