fd21e717370691fa1ca2013b90842bbcd906a9ef
Don Cote <mistercote@gmail.com>
2020-02-19 21:15:41 -0500

    remove test icons/elements

--- src/components/Setlist.tsx
+++ src/components/Setlist.tsx
@@ -5,12 +5,12 @@ import Tracklist, { ITrack } from './Tracklist';
 import Moment from 'react-moment';
 import { Typography, Paper, Link, Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, CardActions, SvgIconProps, SvgIcon, FormControlLabel, Switch, Badge, Tooltip, Checkbox, Grid } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import BeenhereIcon from '@material-ui/icons/Beenhere';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import { red, green } from '@material-ui/core/colors';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ConfirmationNumberIcon from '@material-ui/icons/ConfirmationNumber';
-import RateReviewIcon from '@material-ui/icons/RateReview';
+import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+
 export interface ISetlist {
 	slug: string
 	notes: string
@@ -58,64 +58,19 @@ const Setlist: React.FC<ISetlist> = ({ date, slug, venue, tracks, notes }) => {
 			</CardContent>
 			<CardActions disableSpacing>
 
-			<FormControlLabel
-				value="bottom"
-				control={<Switch color="primary" />}
-				label="Was there"
-				labelPlacement="bottom"
-			/>
-
-			<FormControlLabel
-				value="bottom"
-				control={<Switch color="primary" />}
-				label="Like"
-				labelPlacement="bottom"
-			/>
-
 				<IconButton aria-label="Favorite">
-					<Badge badgeContent={4} color="default">
-					<FavoriteIcon
-						style={{color: red[500] }}
-					/>
-					</Badge>
+					<FavoriteIcon/>
 				</IconButton>
 
 				<IconButton aria-label="I was there">
-					<Badge badgeContent={4} color="default">
-					<ThumbUpIcon />
-					</Badge>
+					<EmojiTransportationIcon/>
 				</IconButton>
-				 <Tooltip title="I was there" aria-label="add">
-					<IconButton aria-label="I was there">
-						<ConfirmationNumberIcon style={{ color: green[500] }} />
-					</IconButton>
-				</Tooltip>
 
 				<IconButton aria-label="Relisten">
 					<HeadsetIcon />
 				</IconButton>
 
 
-				<FormControlLabel
-        control={
-          <Checkbox
-            checked={false}
-            value=""
-            color="default"
-          />
-        }
-        label="I was there"
-      />
-				<FormControlLabel
-        control={
-          <Checkbox
-            checked={true}
-            value=""
-            color="primary"
-          />
-        }
-        label="Like"
-      />
 			</CardActions>
 		</Card>
 	)
