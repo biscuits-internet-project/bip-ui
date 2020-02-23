d830a854d10aab07661d2616544631ea7f9ad0a7
Don Cote <mistercote@gmail.com>
2020-02-23 18:23:48 -0500

    populate the hab page

---
+++ src/components/shared/OperaMenuCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles, CardHeader, List } from '@material-ui/core';
+
+const useStyles = makeStyles({
+	root: {
+    },
+});
+
+const OperaMenuCard = ({title, children}) => {
+    const classes = useStyles()
+    return (
+        <Card>
+            <CardHeader title={title}/>
+            <CardContent>
+                <List>
+                    {children}
+                </List>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default OperaMenuCard
