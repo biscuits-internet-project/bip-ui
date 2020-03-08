af1ff7fe4d82cfa4ac9eeeb932f7579b9140d182
Don Cote <mistercote@gmail.com>
2020-03-07 20:04:19 -0500

    create jam chart hover link component

--- src/components/shows/Tracklist.tsx
+++ src/components/shows/Tracklist.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom'
-import { Popover, Typography, Link, makeStyles, Theme, createStyles, Box } from '@material-ui/core';
+import React from 'react';
+import { Typography, makeStyles, createStyles, Box } from '@material-ui/core';
 import { v4 as uuidv4 } from 'uuid';
+import JamChartHoverLink from './JamChartHoverLink';
 
 export interface ITracklist {
 	tracks: ITrack[]
@@ -22,7 +22,7 @@ export interface ITrack {
 	annotations_with_newlines: string
 }
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
 	createStyles({
 		annotations: {
 			fontStyle: "italic",
@@ -31,35 +31,12 @@ const useStyles = makeStyles((theme: Theme) =>
 		set: {
 			marginTop: 8
 		},
-		paper: {
-			padding: 20,
-			width: 400,
-		  },
-		  popover: {
-			pointerEvents: 'none',
-		  },
 	}),
 );
 
 const Tracklist: React.FC<ITracklist> = ({ tracks }) => {
 	const classes = useStyles();
-	const [openedPopoverId, setOpenedPopoverId] = useState<string | null>(null)
-	const [anchorEl, setAnchorEl] = useState(null)
 	let annLookup = {}
-
-	const handlePopoverOpen = (event, trackSlug) => {
-		// this anchor el doesn't seem like it's getting set properly and
-		// the popover is showing up in the upper left corner of the screen
-		// instead of at the Link element from this event
-		setAnchorEl(event.currentTarget)
-		setOpenedPopoverId(trackSlug)
-	  }
-
-	const handlePopoverClose = () => {
-		console.log(anchorEl)
-		setAnchorEl(null)
-		setOpenedPopoverId(null)
-	};
 
 	const orderTracks = (tracks) => {
 		let sets = {};
@@ -79,47 +56,16 @@ const Tracklist: React.FC<ITracklist> = ({ tracks }) => {
 
 	return (
 		<div>
-			{Object.keys(sets).map((key, i) => {
+			{Object.keys(sets).map((key) => {
 				return (
 					<Typography key={uuidv4()} className={classes.set}>
 						<span style={{ paddingRight: 6 }}>{key} </span>
 						{sets[key].map((track: ITrack, index: number) => {
 							return (
 								<span key={uuidv4()}>
-									<Link
-										on
-										component={RouterLink}
-										to={`/songs/${track.song_slug}`}
-										className={track.note ? "jamchart" : ""}
-										onMouseEnter={track.note ? (e) => handlePopoverOpen(e, track.slug) : undefined}
-										onMouseLeave={track.note ? handlePopoverClose : undefined}
-										aria-owns={openedPopoverId === track.slug ? "foo" : undefined}
-										aria-haspopup="true"
-									>
-										{track.song_title}
-									</Link>
 
-									<Popover
-										id="foo"
-										className={classes.popover}
-										classes={{
-											paper: classes.paper,
-										}}
-										open={openedPopoverId === track.slug}
-										anchorEl={anchorEl}
-										anchorOrigin={{
-											vertical: 'bottom',
-											horizontal: 'center',
-										  }}
-										  transformOrigin={{
-											vertical: 'bottom',
-											horizontal: 'center',
-										  }}
-										onClose={handlePopoverClose}
-										disableRestoreFocus
-									>
-										<Typography>{track.note}</Typography>
-									</Popover>
+									<JamChartHoverLink track={track} />
+
 									{track.annotations.map((a, i) => {
 										return <sup key={i}> {annLookup[a]}</sup>
 									})}
