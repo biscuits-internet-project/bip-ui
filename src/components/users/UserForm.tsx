b34af883e4cabd49387a15faa2c2167e926a05ca
Don Cote <mistercote@gmail.com>
2020-03-14 14:41:44 -0400

    user admin

---
+++ src/components/users/UserForm.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, useContext, useCallback } from 'react';
+import { Form, Formik, FormikProps, FormikHelpers } from 'formik'
+import axios, { AxiosResponse } from 'axios'
+import { useSnackbar } from 'notistack'
+import { AppContext } from '../../context/AppProvider'
+import TextField from '../shared/TextFieldContainer'
+import Button from '@material-ui/core/Button'
+import Uploader from '../shared/Uploader'
+import { Typography } from '@material-ui/core';
+import * as Yup from 'yup';
+import { IUser } from './Users'
+
+interface Props {
+  user?: IUser
+  handleClose: () => void
+}
+
+const initialValues: IUser = {
+  id: "",
+  email: "",
+  username: "",
+  password: "",
+  password_confirmation: "",
+  first_name: "",
+  last_name: "",
+  avatar: "",
+  avatar_url: ""
+}
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required'),
+  username: Yup.string()
+    .required('Username is required'),
+  first_name: Yup.string()
+    .required('First Name is required'),
+  last_name: Yup.string()
+    .required('Last Name is required'),
+});
+
+const UserForm: React.FC<Props> = ({ user, handleClose }) => {
+  const { state } = useContext(AppContext)
+  const [error, setError] = useState(null)
+  const [formData, setFormData] = useState(initialValues)
+  const { enqueueSnackbar } = useSnackbar()
+
+  useEffect(() => {
+    if (user) {
+      setFormData(user)
+    }
+  }, [user])
+
+  const postUser = useCallback(async (values: IUser, actions: FormikHelpers<IUser>) => {
+    console.log(values)
+    try {
+      setError(null)
+      const formData = new FormData();
+      Object.keys(values).forEach(key => formData.append(key, values[key]))
+      await axios({
+        method: user ? 'put' : 'post',
+        url: `${process.env.REACT_APP_API_URL}/users/${user ? user.id : ''}`,
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Authorization": state.token
+        }
+      });
+
+      if (user) {
+        enqueueSnackbar(`Successfully edited user`, { variant: 'success' })
+      } else {
+        enqueueSnackbar(`Successfully added use`, { variant: 'success' })
+      }
+      handleClose()
+    } catch (error) {
+      const { response } = error
+      const { data } = response
+      if (typeof data === 'object') {
+        Object.keys(data).forEach(key => actions.setFieldError(key, data[key]))
+      }
+      else {
+        setError(response.statusText)
+      }
+    }
+  }, [enqueueSnackbar, handleClose, user, state.token])
+
+  return (
+    <Formik
+      enableReinitialize
+      validationSchema={validationSchema}
+      initialValues={formData}
+      onSubmit={(values, actions) => postUser(values, actions)}
+    >
+      {(props: FormikProps<IUser>) => (
+        <Form>
+          <TextField name="email" type="email" label="Email" />
+          <TextField name="username" type="text" label="Username" />
+          <TextField name="first_name" type="text" label="First Name" />
+          <TextField name="last_name" type="text" label="Last Name" />
+          <Uploader {...props} />
+          {error && <Typography color="error" variant="h6">{error}</Typography>}
+          <div style={{ height: 20 }}></div>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+          <div style={{ height: 20 }}></div>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default UserForm
---
+++ src/routing/Routes.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import LatestShows from '../components/shows/LatestShows';
+import Shows from '../components/shows/Shows';
+import Show from '../components/shows/Show';
+import Songs from '../components/songs/Songs';
+import Song from '../components/songs/Song';
+import Venues from '../components/venues/Venues';
+import Venue from '../components/venues/Venue';
+import Register from '../components/Register';
+import Tour from '../components/Tour';
+import Resources from '../components/resources/Resources';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import ResetPassword from '../components/ResetPassword';
+import Login from '../components/Login';
+import BandHistory from '../components/resources/BandHistory'
+import MovieScores from '../components/resources/MovieScores'
+import Tractorbeam from '../components/resources/Tractorbeam'
+import Media from '../components/resources/Media'
+import Touchdowns from '../components/resources/Touchdowns'
+import Perfume from '../components/resources/Perfume'
+import Profile from '../components/Profile';
+import HotAirBalloon from '../components/resources/HotAirBalloon';
+import Gear from '../components/resources/Gear';
+import Music from '../components/resources/Music';
+import ChemicalWarfareBrigade from '../components/resources/ChemcialWarfareBrigade';
+import SideProjects from '../components/resources/SideProjects';
+import AddShow from '../components/shows/AddShow';
+import EditShow from '../components/shows/EditShow';
+import Users from '../components/users/Users';
+
+interface Props {
+	roles: string[]
+}
+
+const Routes: React.FC<Props> = ({roles}) => {
+	return (
+		<React.Fragment>
+			<PrivateRoute path="/admin/shows/create" exact component={AddShow} roles={roles} />
+			<PrivateRoute path="/admin/shows/edit/:id" exact component={EditShow} roles={roles} />
+			<PrivateRoute path="/admin/users" exact component={Users} roles={roles} />
+
+			<Route path="/" exact component={LatestShows} />
+			<Route path="/login" component={Login} />
+			<Route path="/profile" component={Profile} />
+			<Route path="/resources/history" exact component={BandHistory} />
+			<Route path="/resources/gear" exact component={Gear} />
+			<Route path="/resources/music" exact component={Music} />
+			<Route path="/resources/chemical-warfare-brigade" exact component={ChemicalWarfareBrigade} />
+			<Route path="/resources/hot-air-balloon" exact component={HotAirBalloon} />
+			<Route path="/resources/side-projects" exact component={SideProjects} />
+			<Route path="/resources/movie-scores" exact component={MovieScores} />
+			<Route path="/resources/the-perfume" exact component={Perfume} />
+			<Route path="/resources/tractorbeam" exact component={Tractorbeam} />
+			<Route path="/resources/media" exact component={Media} />
+			<Route path="/resources/touchdowns-all-day" exact component={Touchdowns} />
+			<Route path="/shows" exact component={Shows} />
+			<Route path="/shows/add" exact component={AddShow} />
+			<Route path="/shows/:id" exact component={Show} />
+			<Route path="/shows/year/:year" exact component={Shows} />
+			<Route path="/shows/venue/:venue_id" exact component={Shows} />
+			<Route path="/shows/state/:state" exact component={Shows} />
+			<Route path="/songs" exact component={Songs} />
+			<Route path="/songs/:id" component={Song} />
+			<Route path="/venues" exact component={Venues} />
+			<Route path="/venues/:id" exact component={Venue} />
+			<Route path="/tour" exact component={Tour} />
+			<Route path="/register" exact component={Register} />
+			<Route path="/resources" exact component={Resources} />
+			<Route path="/about" exact component={About} />
+			<Route path="/contact" exact component={Contact} />
+			<Route path="/register/confirm" exact component={Register} />
+			<Route path="/password/reset/:token" exact component={ResetPassword} />
+		</React.Fragment>
+	)
+}
+
+export default Routes
