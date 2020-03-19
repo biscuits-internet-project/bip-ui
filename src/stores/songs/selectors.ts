37e3a2e56febea6a97ca5ead33e08bd9755fd9e2
Don Cote <mistercote@gmail.com>
2020-03-19 13:56:49 -0400

    remove curly braces on songs for table selector

--- src/stores/songs/selectors.ts
+++ src/stores/songs/selectors.ts
@@ -7,12 +7,12 @@ export const songsSelector = createSelector(rootSongs, (songs) =>
   Object.values(songs),
 )
 
-export const songsForTableSelector = createSelector(rootSongs, (songs) => {
+export const songsForTableSelector = createSelector(rootSongs, (songs) =>
   Object.values(songs).map((s) => [
     [s.slug, s.title],
     s.author_name,
     s.cover ? 'cover' : 'original',
     s.times_played,
     s.date_last_played,
-  ])
-})
+  ]),
+)
