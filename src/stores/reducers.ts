c84ba07804ef2bd54306d37da6a88a005d214797
Shaun Hutch <shaun@promoboxx.com>
2020-04-13 20:34:29 -0400

    add filter

--- src/stores/reducers.ts
+++ src/stores/reducers.ts
@@ -5,6 +5,7 @@ import { ReviewState } from './reviews/types'
 import { SongState } from './songs/types'
 import { LoadingState } from './loading/types'
 import { BlogState } from './blog/types'
+import { FilterState } from './filters/types'
 
 //Reducers
 import loading from './loading/reducers'
@@ -12,13 +13,18 @@ import venues from './venues/reducers'
 import songs from './songs/reducers'
 import reviews from './reviews/reducers'
 import blog from './blog/reducers'
+import filters from './filters/reducers'
 
 export interface RootState {
   venues: VenueState
   songs: SongState
   loading: LoadingState
   blog: BlogState
+<<<<<<< HEAD
   reviews: ReviewState
+=======
+  filters: FilterState
+>>>>>>> add filter
 }
 
 const rootReducer = combineReducers<RootState>({
@@ -26,7 +32,11 @@ const rootReducer = combineReducers<RootState>({
   venues,
   songs,
   blog,
+<<<<<<< HEAD
   reviews,
+=======
+  filters,
+>>>>>>> add filter
 })
 
 export default rootReducer
