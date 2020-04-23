a056b27eb59d5a10ee122349521fdb41ca8773f6
Shaun Hutch <shaun@promoboxx.com>
2020-04-23 18:42:35 -0400

    persist error

--- src/stores/error/reducers.ts
+++ src/stores/error/reducers.ts
@@ -2,16 +2,12 @@ import { ErrorState } from './types'
 
 const errorReducer = (state: ErrorState = {}, action: any) => {
   const { type, payload } = action
-  const matches = /(.*)_(REQUEST|FULFILLED|REJECTED|CLEAR)/.exec(type)
+  const matches = /(.*)_(REQUEST|FULFILLED|REJECTED)/.exec(type)
 
   if (!matches) return state
 
   const [, requestName, requestState] = matches
-  if (requestState === 'CLEAR') {
-    const newState = { ...state }
-    delete newState[requestName]
-    return newState
-  }
+
   return {
     ...state,
     [requestName]: {
